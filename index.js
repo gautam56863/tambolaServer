43876a8a5eb19a13e25a710ba58b3840a4ac0323
gautam56863 <gautamaggarwal133@gmail.com>
2024-01-23 00:29:39 +0530

    package changes

--- index.js
+++ index.js
@@ -6,53 +6,48 @@ const io = require('socket.io')(4567, {
         origin: ["http://localhost:3000"],
     }
 })
-const ids= new Set();
+const ids = new Set();
 io.on('connection',
     socket => {
-        socket.on("random-number", (num,roomId) => {
-                socket.to(roomId).emit("recieve-random-number", num);
+        socket.on("random-number", (num, roomId) => {
+            socket.to(roomId).emit("recieve-random-number", num);
         })
-        socket.on("Create-room", roomId =>{
-            console.log("create-room"+roomId);
-            const g=ids.has(roomId)
-            if(g==false)
-            {
+        socket.on("Create-room", roomId => {
+            console.log("create-room" + roomId);
+            const g = ids.has(roomId)
+            if (g == false) {
                 ids.add(roomId);
                 socket.join(roomId);
             }
             console.log(ids);
-            if(g==false)
-            {
+            if (g == false) {
                 console.log(1);
-                socket.emit("Room Already Created","Joined Room");
+                socket.emit("Room Already Created", "Joined Room");
             }
-            else
-            {
+            else {
                 console.log(2);
-                socket.emit("Room Already Created","Room Already Created");
+                socket.emit("Room Already Created", "Room Already Created");
             }
             console.log(ids);
         })
-        socket.on("join-room", roomId =>{
+        socket.on("join-room", roomId => {
             // console.log(roomId);
-            if(ids.has(roomId))
-            socket.join(roomId);
-            if(ids.has(roomId))
-            {
-                socket.emit("Error","Joined Room");
+            if (ids.has(roomId))
+                socket.join(roomId);
+            if (ids.has(roomId)) {
+                socket.emit("Error", "Joined Room");
             }
-            else
-            {
-                socket.emit("Error","Incorrect RoomId");
+            else {
+                socket.emit("Error", "Incorrect RoomId");
             }
         })
-        socket.on("delete-room",roomId=>{
+        socket.on("delete-room", roomId => {
             console.log(ids);
             ids.delete(roomId);
             console.log(ids);
             console.log("Entered Delete");
         })
-        socket.on("leave-room",roomId=>{
+        socket.on("leave-room", roomId => {
             console.log("Entered leave");
             socket.leave(roomId);
         })
@@ -64,4 +59,4 @@ io.on('connection',
         // })
     })
 app.get('/', (req, res) => res.send('Server is Running!'))
-app.listen(port,()=> console.log(`Example app listening on port${port}!`))
+app.listen(port, () => console.log(`Example app listening on port${port}!`))
